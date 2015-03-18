2fd44e9fd9b6d8f2feb6f439cb8ad27618327089
Peter Gyory <ptg3662@rit.edu>
2015-03-18 09:41:57 -0400

    Added arena file

---
+++ js/arena.js
@@ -0,0 +1,33 @@
+//Function for making player objects and adding them to the game
+"use strict";
+var game = game || {};
+
+game.createArena = function( color, r, x, y ) {
+  var arena = {
+    x: x,
+    y: y,
+    radius: r,
+    mu : 0.95,
+
+    inBounds : function(player) {
+      var bound = {
+        x: this.x,
+        y: this.y,
+        radius: this.radius - player.radius/5
+      };
+      return game.physicsUtils.circleCollision(bound, player);
+    },
+
+    render : function(ctx) {
+      ctx.save();
+      ctx.fillStyle = this.color;
+      ctx.beginPath();
+      ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+      ctx.closePath();
+      ctx.fill();
+      ctx.restore();
+    }
+  }
+
+  return arena;
+}
