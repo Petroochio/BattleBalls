513e1c0723985f0b385a5b7cd52e2c96bec9688a
Peter Gyory <ptg3662@rit.edu>
2015-04-05 16:06:35 -0400

    Added dash function and draw indicator

--- js/player.js
+++ js/player.js
@@ -10,7 +10,6 @@ game.Player = function() {
     this.yAcc = 0;
     this.velocity = {x: 0, y: 0};
     this.radius = 20;
-    this.speed = 1;
     this.id = id;
     this.color = color;
     this.mu = 0.95;
@@ -22,6 +21,8 @@ game.Player = function() {
     this.coolDown = 0;
     this.stunned = false;
     this.stunTime = 0;
+    this.dashTime = 0;
+    this.dashSpeed = 1;
     this.collisions = [];
   }
 
@@ -29,9 +30,9 @@ game.Player = function() {
 
   p.update = function(dt) {
     this.updateCollisions();
-    this.calculateVelocity(dt);
     this.updateCharge(dt);
     this.updateStun(dt);
+    this.calculateVelocity(dt);
     this.move(dt);
   };
 
@@ -46,7 +47,7 @@ game.Player = function() {
     if(this.stunned)
       this.stunTime--;
     this.stunned = !(this.stunTime <= 0);
-  }
+  };
 
   p.updateCharge = function(dt) {
     if(this.charging)
@@ -84,9 +85,29 @@ game.Player = function() {
   };
 
   p.doDash = function() {
-
+    var aX = this.xAcc * this.charge/maxCharge * 10;
+    var aY = this.yAcc * this.charge/maxCharge * 10;
+    this.updateAcceleration(aX, aY);
+    this.charge = 0;
+    this.charging = false;
+    this.coolDown = 20;
+  };
+  /*
+  p.startDash = function() {
+    this.dashTime = this.charge/maxCharge * 2;
+    this.
   };
 
+  p.updateDash = function(dt) {
+    this.dashTime--;
+    if(this.dashTime <= 0)
+      this.endDash();
+  };
+
+  p.endDash = function() {
+    this.
+  };
+  */
   p.updateCollisions = function() {
     var self = this;
     this.collisions.forEach(function(ball, index, array){
@@ -167,7 +188,26 @@ game.Player = function() {
     ctx.closePath();
     ctx.stroke();
     ctx.restore();
-  }
+  };
   
+  p.drawDashCharge = function(ctx) {
+    var forward = { x: this.velocity.x, y: this.velocity.y };
+    forward = game.physicsUtils.normalize(forward);
+
+    ctx.save();
+    ctx.fillStyle = this.color;
+    ctx.strokeStyle = this.color;
+    ctx.lineWidth = 3;
+    ctx.shadowBlur=10;
+    ctx.shadowColor=this.color;
+    ctx.beginPath();
+    ctx.moveTo(this.x, this.y);
+    ctx.lineTo(this.x + forward.x * this.charge/this.maxCharge * 10,
+      this.y + forward.y * this.charge/this.maxCharge * 10,)
+    ctx.closePath();
+    ctx.stroke();
+    ctx.restore();
+  };
+
   return Player;
 }();
