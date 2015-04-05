cfda415c270ffa38fd6c38a7ab5e683f4cea0510
Peter Gyory <ptg3662@rit.edu>
2015-04-05 17:10:52 -0400

    Call all socket functions in socket handler

--- js/socketHandlers.js
+++ js/socketHandlers.js
@@ -1,30 +1,37 @@
-me.canvas = document.querySelector('#area');
-me.ctx = me.canvas.getContext('2d');
-me.ctx.lineWidth = 5;
-me.arena = game.createArena('white', /*230*/me.canvas.height/2-10, me.canvas.width/2, me.canvas.height/2);
+"use strict";
+var game = game || {};
 
-//Set up socket events --Ha Andrew don't look at this --You can't stop me
-socket.on('player join', function(data){
-  var x = me.canvas.width/2, y = me.canvas.height/2;
-  me.players[data.id] = new game.Player(data.id, data.color, x, y);
-  me.playerIDs.push(data.id);
-});
+game.socketHandlers = {
+  init : function(app) {
+    /*me.canvas = document.querySelector('#area');
+    me.ctx = me.canvas.getContext('2d');
+    me.ctx.lineWidth = 5;
+    me.arena = game.createArena('white', me.canvas.height/2-10, me.canvas.width/2, me.canvas.height/2);*/
 
-socket.on('player leave', function(data){
-  me.playerIDs.splice(me.playerIDs.indexOf(data.id),1);
-  delete me.players[data.id];
-});
+    //Set up socket events --Ha Andrew don't look at this --You can't stop me
+    socket.on('player join', function(data){
+      var x = app.canvas.width/2, y = app.canvas.height/2;
+      app.players[data.id] = new game.Player(data.id, data.color, x, y);
+      app.playerIDs.push(data.id);
+    });
 
-socket.on('charge start', function(data){
-  me.players[data.id].beginCharge();
-});
+    socket.on('player leave', function(data){
+      app.playerIDs.splice(app.playerIDs.indexOf(data.id),1);
+      delete app.players[data.id];
+    });
 
-socket.on('charge end', function(data){
-  me.players[data.id].endCharge();
-});
+    socket.on('charge start', function(data){
+      app.players[data.id].beginCharge();
+    });
 
-socket.on('phone tilt', function(data) {
-  if(me.players[data.id]) {
-    me.players[data.id].updateAcceleration(data.yAcc/300, -data.xAcc/300);
-  } //my eyes are everywhere --I will gouge your eyes out
-});
+    socket.on('charge end', function(data){
+      app.players[data.id].endCharge();
+    });
+
+    socket.on('phone tilt', function(data) {
+      if(app.players[data.id]) {
+        app.players[data.id].updateAcceleration(data.yAcc/300, -data.xAcc/300);
+      } //my eyes are everywhere --I will gouge your eyes out
+    });
+  }
+}
