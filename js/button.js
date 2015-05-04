e4799bd192e6c5a9707f5f0448937c5d677951c5
Andrew Ahlquist <aga8640@rit.edu>
2015-05-04 01:51:38 -0400

    Added button size changing logic

--- js/button.js
+++ js/button.js
@@ -53,7 +53,11 @@ game.Button = function() {
     var p = Button.prototype;
 
     p.update = function(dt) {        
-        
+        if(game.controller.touching &&
+           game.controller.xTap >= this.x-this.radius && game.controller.xTap <= this.x+this.radius &&
+           game.controller.yTap >= this.y-this.radius && game.controller.yTap <= this.y+this.radius){
+            this.currentlyPressed = true;
+        }else if(!game.controller.touching) this.currentlyPressed = false;
         
     };
     
@@ -73,20 +77,22 @@ game.Button = function() {
             this.ctx.save();
             this.ctx.lineWidth = 5;
             this.ctx.shadowBlur= 15;
-            
+            this.ctx.shadowColor = this.color;
+
             //draw based on current press state
             if(this.currentlyPressed != true)
             {
-                this.ctx.shadowColor = this.color;
+//                this.ctx.shadowColor = this.color;
                 game.DrawLib.drawCircle(this.ctx, this.x, this.y, this.radius, 'black', this.color);
                 game.DrawLib.drawText(this.ctx, this.id, this.x, this.y+35, 80, this.color);
             }
             else{
-                this.ctx.shadowColor = 'white';
-                game.DrawLib.drawCircle(this.ctx, this.x, this.y, this.width, this.height, this.color, 'white');
-                game.DrawLib.drawText(this.ctx, this.id, this.x, this.y + this.height/5, 40, 'white');
+//                this.ctx.shadowColor = 'white';
+                game.DrawLib.drawCircle(this.ctx, this.x, this.y, this.radius-this.radius/4, 'black', this.color);
+                game.DrawLib.drawText(this.ctx, this.id, this.x, this.y+25, 60, this.color);
             }
         this.ctx.restore();
+//        console.log(this.currentlyPressed);
 
 //        }
     }
