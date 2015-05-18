8cc83b69941be6aa207f12be3a0da3487671216f
Peter Gyory <ptg3662@rit.edu>
2015-05-18 19:31:18 -0400

    Added greek letters to display class

--- assets/js/matador.js
+++ assets/js/matador.js
@@ -215,10 +215,8 @@ game.Matador = function() {
     ctx.lineWidth = 3;
     ctx.shadowBlur=10;
     ctx.shadowColor=this.color;
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-    ctx.closePath();
-    ctx.stroke();
+    game.DrawLib.drawCircle(ctx, this.x, this.y, this.radius, "black", this.color);
+    game.DrawLib.drawText(ctx, "Ω", this.x, this.y + this.radius/2, 40, this.color);
     ctx.restore();
     if(this.charging)
       this.renderCharge(ctx);
